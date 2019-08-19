653a24f3d6549ad41cfa68f4b16cc17c4b196068
양희철 <heecheol1508@gmail.com>
2019-08-19 16:16:26 +0900

    0819

--- d6/1267.py
+++ d6/1267.py
@@ -2,7 +2,7 @@ import sys
 sys.stdin = open('input.txt', 'r')
 import copy
 
-for tc in range(1, 11):
+for tc in range(5):
 
     V, E = map(int, input().split())
 
@@ -19,37 +19,50 @@ for tc in range(1, 11):
     result = [order[0][0], order[0][1]]
     last_order = []
 
-
-
     for i in range(1, E):
         # print(order[i][0], order[i][1])
         if order[i][0] in result and order[i][1] in result:
             pass
+
         elif order[i][0] in result and order[i][1] not in result:
             k = result.index(order[i][0])
             result.insert(k+1, order[i][1])
             result[k+1] = order[i][1]
+
         elif order[i][0] not in result and order[i][1] in result:
             k = result.index(order[i][1])
-            result.insert(k+1, order[i][0])
+            result.insert(k, order[i][0])
+
         else:
             last_order.append([order[i][0], order[i][1]])
-
+    print('===============================================')
+    print(result)
+    print(last_order)
+    
     while len(last_order) > 0:
+        temp2 = len(last_order)
         order = copy.deepcopy(last_order)
         last_order = []
         for i in range(len(order)):
             if order[i][0] in result and order[i][1] in result:
                 pass
+
             elif order[i][0] in result and order[i][1] not in result:
                 k = result.index(order[i][0])
                 result.insert(k+1, order[i][1])
                 result[k+1] = order[i][1]
+
             elif order[i][0] not in result and order[i][1] in result:
                 k = result.index(order[i][1])
-                result.insert(k+1, order[i][0])
+                result.insert(k, order[i][0])
+                
             else:
                 last_order.append([order[i][0], order[i][1]])
 
-    reresult = ' '.join(result)
-    print(f'#{tc} {reresult}')
+        temp = len(last_order)                
+        if temp == temp2 and temp != 0:
+            result.append(last_order[0][0])
+            result.append(last_order[0][1])
+    
+    # reresult = ' '.join(result)
+    # print(f'#{tc} {reresult}')
