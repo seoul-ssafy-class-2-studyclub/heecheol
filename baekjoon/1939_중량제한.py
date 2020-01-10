0a6fafc667396f4c66a4a72bf7b25aa845409a83
heecheol <heecheol1508@gmail.com>
2020-01-10 09:50:44 +0900

    0110

--- baekjoon/1939_중량제한.py
+++ baekjoon/1939_중량제한.py
@@ -4,19 +4,41 @@ sys.stdin = open('input.txt', 'r')
 
 
 N, M = map(int, input().split())
-nodes = [[] for _ in range(N + 1)]
-weights = [[0] * (N + 1) for _ in range(N + 1)]
+nodes = [{} for _ in range(N + 1)]
 
 for _ in range(M):
     A, B, C = map(int, input().split())
-    nodes[A].append(B)
-    nodes[B].append(A)
-    weights[A][B] = weights[B][A] = C
+    if nodes[A].get(B) is None:
+        nodes[A][B] = nodes[B][A] = C
+    else:
+        if nodes[A][B] < C:
+            nodes[A][B] = nodes[B][A] = C
 
 start_node, end_node = map(int, input().split())
 
 visit = [False] * (N + 1)
-# nodes[start_node]
+visit[start_node] = True
 
 queue = []
+for key, value in nodes[start_node].items():
+    heapq.heappush(queue, (-value, key))
 
+answer = float('inf')
+flag = True
+while True:
+    possible_weight, island = heapq.heappop(queue)
+    if visit[island] is True:
+        continue
+    visit[island] = True
+
+    if -possible_weight < answer:
+        answer = -possible_weight
+
+    if island == end_node:
+        break
+
+    for key, value in nodes[island].items():
+        if visit[key] is False:
+            heapq.heappush(queue, (-value, key))
+
+print(answer)
