b85613cf4d5aa72121239ec218273ea5b1740444
heecheol <heecheol1508@gmail.com>
2020-01-23 19:02:55 +0900

    0123

--- baekjoon/18224_미로에 갇힌 건우.py
+++ baekjoon/18224_미로에 갇힌 건우.py
@@ -1,49 +1,40 @@
 import sys
-sys.stdin = open('input.txt', 'r')
-# input = sys.stdin.readline
+# sys.stdin = open('input.txt', 'r')
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
 
-N, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
+def solution(idx1, idx2, cnt, is_sun):
+    global shortcut
+    global answer
 
-adj_list = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-visit = [[False] * N for _ in range(N)]
-queue = [[0, 0, True, 0, 1]]
-arrived = False
-while queue:
-    idx1, idx2, is_sun, m, day = queue.pop(0)
-
-    if m == 0 or m % M != 0:
-        pass
-    else:
-        if is_sun is True:
-            is_sun = False
-        else:
-            day += 1
-            is_sun = True
-    # print()
-    # print(idx1, idx2, is_sun)
+    if cnt >= shortcut:
+        return
     if idx1 == idx2 == N - 1:
-        arrived = True
-        break
+        shortcut = cnt
+        if is_sun == 1:
+            answer = 'sun'
+        else:
+            answer = 'moon'
+        return
 
-    if visit[idx1][idx2] is False:
-        visit[idx1][idx2] = True
-
-        # print(idx1, idx2, m, is_sun)
-        for adj in adj_list:
-            nxt1, nxt2 = idx1 + adj[0], idx2 + adj[1]
-            if 0 <= nxt1 < N and 0 <= nxt2 < N:
-                if visit[nxt1][nxt2] is True:
-                    continue
-                elif board[nxt1][nxt2] == 0:
-                    queue.append([nxt1, nxt2, is_sun, m + 1, day])
-                else:
-                    if is_sun is True:
-                        continue
-
+    will_sun = is_sun
+    for adj in adj_list:
+        nxt1, nxt2 = idx1 + adj[0], idx2 + adj[1]
+        if 0 <= nxt1 < N and 0 <= nxt2 < N:
+            if visit[nxt1][nxt2] is True:
+                continue
+            if board[nxt1][nxt2] == 0:
+                visit[nxt1][nxt2] = True
+                if cnt % M == 0:
+                    will_sun = -(is_sun - 1)
+                solution(nxt1, nxt2, cnt + 1, will_sun)
+                visit[nxt1][nxt2] = False
+            else:
+                if is_sun == 0:
                     while True:
-                        nxt1, nxt2 = nxt1 + adj[0], nxt2 + adj[1]
+                        nxt1 += adj[0]
+                        nxt2 += adj[1]
                         if 0 <= nxt1 < N and 0 <= nxt2 < N:
                             if board[nxt1][nxt2] == 1:
                                 continue
@@ -51,15 +42,32 @@ while queue:
                                 if visit[nxt1][nxt2] is True:
                                     break
                                 else:
-                                    queue.append([nxt1, nxt2, is_sun, m + 1, day])
+                                    visit[nxt1][nxt2] = True
+                                    if cnt % M == 0:
+                                        will_sun = -(is_sun - 1)
+                                    solution(nxt1, nxt2, cnt + 1, will_sun)
+                                    visit[nxt1][nxt2] = False
                                     break
                         else:
                             break
 
-if arrived is True:
-    if is_sun:
-        print(day, 'sun')
-    else:
-        print(day, 'moon')
-else:
+
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+
+adj_list = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+visit = [[False] * N for _ in range(N)]
+visit[0][0] = True
+
+shortcut = float('inf')
+answer = 0
+solution(0, 0, 1, 1)    # idx1, idx2, cnt, is_sun
+
+if answer == 0:
     print(-1)
+else:
+    if shortcut % (2 * M) == 0:
+        day = shortcut // (2 * M)
+    else:
+        day = (shortcut // (2 * M)) + 1
+    print(day, answer)
