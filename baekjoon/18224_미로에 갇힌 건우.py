24c4d3b09b28655f56e26bded65d16c86d1aafcc
heecheol <heecheol1508@gmail.com>
2020-01-26 17:29:54 +0900

    0126

--- baekjoon/18224_미로에 갇힌 건우.py
+++ baekjoon/18224_미로에 갇힌 건우.py
@@ -1,73 +1,72 @@
+# from pprint import pprint
+import collections
 import sys
 # sys.stdin = open('input.txt', 'r')
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-
-def solution(idx1, idx2, cnt, is_sun):
-    global shortcut
-    global answer
-
-    if cnt >= shortcut:
-        return
-    if idx1 == idx2 == N - 1:
-        shortcut = cnt
-        if is_sun == 1:
-            answer = 'sun'
-        else:
-            answer = 'moon'
-        return
-
-    will_sun = is_sun
-    for adj in adj_list:
-        nxt1, nxt2 = idx1 + adj[0], idx2 + adj[1]
-        if 0 <= nxt1 < N and 0 <= nxt2 < N:
-            if visit[nxt1][nxt2] is True:
-                continue
-            if board[nxt1][nxt2] == 0:
-                visit[nxt1][nxt2] = True
-                if cnt % M == 0:
-                    will_sun = -(is_sun - 1)
-                solution(nxt1, nxt2, cnt + 1, will_sun)
-                visit[nxt1][nxt2] = False
-            else:
-                if is_sun == 0:
-                    while True:
-                        nxt1 += adj[0]
-                        nxt2 += adj[1]
-                        if 0 <= nxt1 < N and 0 <= nxt2 < N:
-                            if board[nxt1][nxt2] == 1:
-                                continue
-                            else:
-                                if visit[nxt1][nxt2] is True:
-                                    break
-                                else:
-                                    visit[nxt1][nxt2] = True
-                                    if cnt % M == 0:
-                                        will_sun = -(is_sun - 1)
-                                    solution(nxt1, nxt2, cnt + 1, will_sun)
-                                    visit[nxt1][nxt2] = False
-                                    break
-                        else:
-                            break
 
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
-adj_list = [(0, -1), (-1, 0), (0, 1), (1, 0)]
-visit = [[False] * N for _ in range(N)]
-visit[0][0] = True
+adj_list = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-shortcut = float('inf')
-answer = 0
-solution(0, 0, 1, 1)    # idx1, idx2, cnt, is_sun
+queue = collections.deque([(0, 0)])
+is_moon = 0
+cnt = 1
 
-if answer == 0:
-    print(-1)
-else:
-    if shortcut % (2 * M) == 0:
-        day = shortcut // (2 * M)
+flag = False
+visit = [[[-1] * M for _ in range(N)] for _ in range(N)]
+
+while queue:
+
+    for _ in range(len(queue)):
+        idx1, idx2 = queue.popleft()
+        if idx1 == idx2 == N - 1:
+            flag = True
+            break
+
+        for adj in adj_list:
+            nxt1, nxt2 = idx1 + adj[0], idx2 + adj[1]
+            if 0 <= nxt1 < N and 0 <= nxt2 < N:
+                if board[nxt1][nxt2] == 0:
+                    if visit[nxt1][nxt2][cnt % M] == is_moon or visit[nxt1][nxt2][cnt % M] == 2:
+                        continue
+                    if is_moon == 0:
+                        visit[nxt1][nxt2][cnt % M] += 1
+                    else:
+                        visit[nxt1][nxt2][cnt % M] += 2
+                    queue.append((nxt1, nxt2))
+                else:
+                    if is_moon == 1:
+                        while True:
+                            nxt1 += adj[0]
+                            nxt2 += adj[1]
+                            if 0 <= nxt1 < N and 0 <= nxt2 < N:
+                                if board[nxt1][nxt2] == 1:
+                                    continue
+                                else:
+                                    if visit[nxt1][nxt2][cnt % M] == is_moon or visit[nxt1][nxt2][cnt % M] == 2:
+                                        break
+                                    visit[nxt1][nxt2][cnt % M] += 2
+                                    queue.append((nxt1, nxt2))
+                                    break
+                            else:
+                                break
+    # pprint(visit)
+
+    if flag is True:
+        break
+    if cnt % M == 0:
+        is_moon = -(is_moon - 1)
+    cnt += 1
+# print(cnt)
+
+if flag is True:
+    if is_moon == 0:
+        answer = 'sun'
     else:
-        day = (shortcut // (2 * M)) + 1
+        answer = 'moon'
+    day = (cnt - 1) // (2 * M) + 1
     print(day, answer)
+else:
+    print(-1)
